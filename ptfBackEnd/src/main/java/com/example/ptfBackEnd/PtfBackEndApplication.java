1e025a98a3f630d9d31b50e801eb08b8a0962be8
ViniciusGr63 <128187154+ViniciusGr63@users.noreply.github.com>
2025-02-20 21:20:03 +0000

    primeiras configurações

---
+++ ptfBackEnd/src/main/java/com/example/ptfBackEnd/PtfBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.ptfBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtfBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtfBackEndApplication.class, args);
+	}
+
+}
