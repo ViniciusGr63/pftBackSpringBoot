e32e55bdc009d619df44d261086780c81ab860a8
ViniciusGr63 <128187154+ViniciusGr63@users.noreply.github.com>
2025-02-20 22:40:40 +0000

    imagem

---
+++ ptfBackEnd/src/test/java/com/example/ptfBackEnd/PtfBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ptfBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtfBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
