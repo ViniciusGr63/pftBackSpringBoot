5e4d94ef38d876d992650423104b5fe283eef265
ViniciusGr63 <128187154+ViniciusGr63@users.noreply.github.com>
2025-02-24 19:19:21 +0000

    tst

--- src/main/java/com/example/ptfBackEnd/controllers/WebController.java
+++ src/main/java/com/example/ptfBackEnd/controllers/WebController.java
@@ -2,16 +2,12 @@ package com.example.ptfBackEnd.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
     
-    private final String msg = "Ti amu";
-    
     @GetMapping("/msg")
-    @ResponseBody 
     public String mostrarPagina() {
-        return msg;  
+        return "Pagina";  // Nome do arquivo HTML sem a extensão ".html"
     }
 }
